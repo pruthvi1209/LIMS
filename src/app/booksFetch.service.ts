f6c7336c2f3716a05220c248bfbc449f2fcfdee9
MINDTREE\M1043150 <Pruthivi.ladhipeerla@mindtree.com>
2018-11-29 11:18:35 +0530

    Modified search and Add book with ISBN Number

--- src/app/booksFetch.service.ts
+++ src/app/booksFetch.service.ts
@@ -3,13 +3,13 @@ import { Http, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { Books } from './dataModels/books.model';
+import { UIService } from '../shared/ui.service';
 
 @Injectable()
 
 export class BooksFetch {
-    constructor(private http: Http) { }
+    constructor(private http: Http, private uiService: UIService) { }
     allbooks = [];
-    // filteredBooks = new Subject<any[]>();
     getAllBooks() {
         return this.http.get('https://my-libarary.firebaseio.com/books.json').pipe(map((response: Response) => {
             const data = response.json();
@@ -28,5 +28,27 @@ export class BooksFetch {
             });
         });
 }
+    addBook(newBook){
+        this.getAllBooks().subscribe((books)=>{
+            let duplicateBook = false;
+            books.forEach((book)=>{
+                if(book.isbn=== newBook.isbn){
+                    duplicateBook = true;
+                }
+            })
+            if(duplicateBook){
+            this.uiService.showSnackar('Book Already Exsits', null , 3000);
+            return;
+            } else{
+                books.push(newBook)
+                this.http.put('https://my-libarary.firebaseio.com/books.json',books).subscribe((value) =>{
+                if(value.status === 200) {
+                this.uiService.showSnackar('Book added Sucessfully',null,4000);
+                }
+            },
+            (e) =>{ this.uiService.showSnackar(e.message,null,3000)});
+            }
+        })
+    }
 }
 
